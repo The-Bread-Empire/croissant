19a913f9a3de09509c0cef47e212c3fbc793fefd
Aiden Favish <aidenfavish@gmail.com>
2020-10-11 01:23:40 +0000

    new layout

---
+++ eventcommands.py
@@ -0,0 +1,65 @@
+import discord
+import os
+import random
+import time
+from time import sleep
+from make_website import make_website
+
+from commands import clear
+from discord.ext import commands
+import discord.utils
+from datetime import datetime
+from os import system, name
+
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+  ch = message.content
+  re = message.channel.send
+  if "lonley" in ch: re("hey die alone")
+
+eventcommands = {
+  "lonley":"hey, die alone",
+  "I hate you":"Ungrateful loser",
+  "test":"I am dead inside",
+  "sus":"nah u vented no cap",
+  "what":"Learn or I will chew your eyeballs out",
+  "no":"BUT WHY",
+  "wtf":"lmao this asshole",
+  "bruh":"bruh bruh",
+  "bae":"wassup my shawty Bae",
+  "stfu":"why dipstick",
+  "ahaha":"alive, ahaha why q-q",
+  "lol":"haha a laughing loser",
+  "wow":"ur stoopid",
+  "yes":"r u sure",
+  "i am b":"let me go grab the spatchula",
+  "nice":"NOICE",
+  "lmao":"wow what a dipstick",
+  "Marmu":"eww a worm",
+  "yes, my life":"oh, OH SHIZ",
+  "yes, inside":"Same q-q",
+  "Demon Cheese": "Only the best cheese possible",
+  "what are you doing?":"Illigal doings don't call the police",
+  "Markoth":"Alexa play Mothman",
+  "The collector":"H A N D S",
+  "Pure Vessel":"oh that mothertrucker",
+  "Groz Mother":"He do be sleeping doe",
+  "No eyes":"lel imaginge not being able to see",
+  "Broken Vessel":"He do be looking like a lava lamp",
+  "Xero":"Mr tentacle man",
+  "Paintmaster Sheo":"r u gonna paint me like ur french girls?",
+  "Massive Moss Charger (aka: the bush)":"B U S H M A N",
+  "Gorb":"Micro worm butt",
+  "Soul Master":"ur a wizzard harry",
+  "Galien":"CRAB MEAT",
+  "Dung Defender":"He yeets poopoo",
+  "Nightmare King Grim":"Stoopid vampire man",
+  "Winged Nosk":"My pet, miNEEE",
+  "Crystal Guardian":"roCk",
+  "Uumuu":"UWU ^v^  i want to die now",
+  "Hive knight":"Swarm of death bees rain from above!"
+
+}
--- main.py
+++ main.py
@@ -4,108 +4,111 @@ import random
 import time
 from time import sleep
 from make_website import make_website
+from eventcommands import eventcommands
 from commands import clear
 from discord.ext import commands
 import discord.utils
 from datetime import datetime
-from os import system, name 
+from os import system, name
+
 #this is just certain dependencies that we may use later very standered
 gamesc = 0
-gamesr = random.randint(4, 5)
+gamesr = random.randint(3, 9)
+
+
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
-  clear()
-  sleep(1)
-  print("I'm online")
-  sleep(1)
-  print(f"username: {client.user}")
+    clear()
+    sleep(1)
+    print("I'm online")
+    sleep(1)
+    print(f"username: {client.user}")
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you"))
 
-lonley = "lonley"
-lol = "lol"
-wow = "wow"
-yes = "yes"
-nice = 'nice'
-wtf = "wtf"
-lmao = "lmao"
-bae = "bae"
+
 fish = "fish"
-no = "no "
-I_am_b = "i am b"
-
+rank = "!rank"
 @client.event
 async def on_message(message):
-  randomspeak = random.randint(1, 3)
-  if randomspeak == 1:
-    if message.author.id == 459905208160616459:
-      await message.delete()
-    #await message.channel.send("Your mean")
-      print(f"{message.author} said: {message.content} (message suppresed)")
-    if message.content == "test":
-      await message.channel.send("I am dead inside")
-    elif message.content == "what":
-      await message.channel.send("learn or I will chew ur eye balls")
-    if no in message.content:
-      await message.channel.send("BUT WHY")
-    elif message.content == "bruh":
-      await message.channel.send("bruh bruh")
-    if wtf in message.content:
-      await message.channel.send("lmao this asshole")
-    if bae in message.content:
-      await message.channel.send("wassup my shawty Bae")
-    elif message.content == "stfu":
-      await message.channel.send("why dipshit")
-    elif lol in message.content:
-      await message.channel.send("Haha a laughing loser")
-    if wow in message.content:
-      await message.channel.send("Ur stoopid")
-    if yes in message.content:
-      await message.channel.send("r u sure ")
-    if I_am_b in message.content:
-      await message.channel.send ("let me go grab the spatchula")
-    if nice in message.content:
-      await message.channel.send("NOICE")
-    if lmao in message.content: 
-      await message.channel.send("wow what a dipstick")
     
-    if fish in message.content:
-      if message.content == "fishy sticks":
-        print("spam prevented")
-      else:
-        await message.channel.send("fishy sticks")
-    elif message.content == "I hate you":
-      await message.channel.send("Ungrateful loser")
-  
-    if lonley in message.content:
-      await message.channel.send("hey, die alone")
-    
-    if (message.content == "hello") or (message.content == "hi"):
-      hellorand = random.randint(1, 5)
-      if hellorand == 1:
-        await message.channel.send("howdy")
-      elif hellorand == 2:
-        await message.channel.send("lemme in ur house")
-      elif hellorand == 3:
-        await message.channel.send("go die")
-      elif hellorand == 4:
-        await message.channel.send("nOOO")
-      else:
-        await message.channel.send("im out, NOPE")
+
+    status = random.randint(1, 5)
+    if status == 1:
+      await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you eat"))
+    elif status == 2:
+      await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you sleep"))
+    elif status == 3:
+      await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you piss"))
+    elif status == 4:
+      await client.change_presence(activity=discord.Game(name="roasting you"))
+    else:
+      await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you"))
     randomspeak = random.randint(1, 2)
-  if message.content == "!rank":
-    await message.channel.send ("your a dissapointment")
-  if (message.content == "games") and (message.channel.id == 761325921029586974):
-    global gamesc
-    global gamesr
-    gamesc = gamesc + 1
-    print(gamesc)
+    if randomspeak == 1:
+        if message.content in eventcommands:
+          await message.channel.send(eventcommands[message.content])
+          #about 40 of the commands come from th 2 lines of code above this comment
+        
+        if fish in message.content:
+            if message.content == "fishy sticks":
+                print("spam prevented")
+            else:
+                await message.channel.send("fishy sticks")
+
+
+        if (message.content == "hello") or (message.content == "hi"):
+            hellorand = random.randint(1, 5)
+            if hellorand == 1:
+                await message.channel.send("howdy")
+            elif hellorand == 2:
+                await message.channel.send("lemme in ur house")
+            elif hellorand == 3:
+                await message.channel.send("don't bother me")
+            elif hellorand == 4:
+                await message.channel.send("nOOO")
+            else:
+                await message.channel.send("im out, NOPE")
     
-    print(f"{gamesr} till next games")
-    if gamesc >= gamesr:
-      gamesc = 0
-      await message.channel.send("games")
-      gamesr = random.randint(3, 9)
+        randomspeak = random.randint(1, 2)
+    if rank in message.content:
+      print(f"{message.author} sent the message")
+      if message.content == "!rank":
+        await message.channel.send("your a dissapointment")
+      else:
+       print("Someone is trying to surpass you")
+    
+    if (message.content == "games") and (
+            message.channel.id == 761325921029586974):
+        global gamesc
+        global gamesr
+        gamesc = gamesc + 1
+        print(gamesc)
+
+        print(f"{gamesr} till next games")
+        if gamesc >= gamesr:
+            gamesc = 0
+            await message.channel.send("games")
+            gamesr = random.randint(3, 9)
+
+    if message.content == "Croissant eat the chat":
+      for i in range(100):
+        if message.author.id == 547942699572002836: 
+          await message.channel.send("** ** \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n ** **")
+          
+    if message.author.id == 424413952559153178:
+      if message.guild.id == 691024705905229945:
+        print("suppression disabled")
+      else:
+        
+      #await message.channel.send("Your mean")
+        print(f"{message.author} said: {message.content} (message suppresed in {message.channel.name} channel in the {message.guild.name} server)")
+    if message.author.id == 589309749141569554:
+      await message.channel.send(f"Shut up {message.author.name} or I will yeet a snake at you")
+    
+ 
 
 
 #below is just certain commands connecting the bot to the interent and connecting it to discord
